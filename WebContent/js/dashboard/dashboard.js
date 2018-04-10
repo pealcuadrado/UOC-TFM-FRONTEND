2ee6dd6a240eec43eeeb74dab6da14834a99d7a2
josejimenez <josejl92@gmail.com>
2018-04-10 23:19:03 +0200

    inicio dashboard con angular.js

--- WebContent/js/dashboard/dashboard.js
+++ WebContent/js/dashboard/dashboard.js
@@ -1,5 +1,7 @@
 var url="http://localhost:8080/";
-$(document).ready(function() {
+
+var listoplan= angular.module('listoplan',[]);
+listoplan.controller('datosUsuarioController', function($scope,$http) {
 	token=localStorage.getItem("token");
 	if(token==null){
 		console.log("Token no disponible");
@@ -8,22 +10,59 @@ $(document).ready(function() {
 	else{
 		console.log(token);
 	}
-    $.ajax({
+    /*$.ajax({
     	type:"get",
     	url:url+"usuarios/informacion_usuario/0",
     	headers: {"token":token},
     	dataType:"json",
     	contentType: "application/json; charset=utf-8"
     }).done(function(data){
-    	$("#contenido").text("Hola " + data.nombre + " " + data.apellido + " (" + data.id_usuario + ")")
+    	$scope.$apply(function(){
+        	$scope.nombre=data.nombre;
+        	$scope.apellido=data.apellido;
+        	$scope.id_usuario=data.id_usuario;
+    	});
     }).fail(function(data, statusText, xhr){
     	console.log(data.status);
     	window.location.replace("login.html");
-    });
+    });*/
+    
+    $http({
+    	  method: 'GET',
+    	  url: url+"usuarios/informacion_usuario/0",
+    	  headers: {"token":token},
+    	}).then(function successCallback(response) {
+        	$scope.nombre=response.data.nombre;
+        	$scope.apellido=response.data.apellido;
+        	$scope.id_usuario=response.data.id_usuario;
+    	  }, function errorCallback(response) {
+    		  	console.log(response.data.status);
+    	    	window.location.replace("login.html");
+    	  });
 });
+
+listoplan.controller('modalController', function($scope) {
+	$scope.refrescarModal= function(elemento){
+		if(elemento=="nota"){
+			$("#contenido_modal").load("modals/nota.html");
+			$scope.modal={"titulo":"Nota", "boton":"Guardar nota"};
+		}
+		if(elemento=="lista"){
+			$("#contenido_modal").load("modals/lista.html");
+			$scope.modal={"titulo":"Lista", "boton":"Guardar lista"};
+		}
+	}
+})
+
 
 $("#logout").click(function(evento){
 	evento.preventDefault();
 	localStorage.removeItem('token');
 	window.location.replace("login.html");
 });
+
+/*$("#nueva_nota").click(function(evento){
+	evento.preventDefault();
+	$("#contenido_modal").load("modals/nota.html");
+	$("#contenido_")
+});*/
