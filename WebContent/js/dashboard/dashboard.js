e499d3cb67d33cd92c5b37287aaaa5fafcddda68
josejimenez <josejl92@gmail.com>
2018-04-14 14:21:19 +0200

    14-4-2018

--- WebContent/js/dashboard/dashboard.js
+++ WebContent/js/dashboard/dashboard.js
@@ -19,22 +19,6 @@ listoplan.controller('datosUsuarioController', function($scope,$http,InfoUsuario
 		console.log(token);
 		InfoUsuario.token=token;
 	}
-    /*$.ajax({
-    	type:"get",
-    	url:url+"usuarios/informacion_usuario/0",
-    	headers: {"token":token},
-    	dataType:"json",
-    	contentType: "application/json; charset=utf-8"
-    }).done(function(data){
-    	$scope.$apply(function(){
-        	$scope.nombre=data.nombre;
-        	$scope.apellido=data.apellido;
-        	$scope.id_usuario=data.id_usuario;
-    	});
-    }).fail(function(data, statusText, xhr){
-    	console.log(data.status);
-    	window.location.replace("login.html");
-    });*/
     
     $http({
     	  method: 'GET',
@@ -53,23 +37,43 @@ listoplan.controller('datosUsuarioController', function($scope,$http,InfoUsuario
     	  });
 });
 
-listoplan.controller('modalController', function($scope) {
-	$scope.refrescarModal= function(elemento){
+listoplan.controller('modalController', function($scope,$http,InfoUsuario) {
+	$scope.refrescarModal= function(elemento,id){
 		if(elemento=="nota"){
 			$scope.plantilla='modals/nota.html';
 			//$("#contenido_modal").load("modals/nota.html");
 			$scope.modal={"titulo":"Nota"};
+			//si tiene id, recuperar la información para visualizar o modificar
+			if(id > 0){
+		        $http({
+			      	  method: 'GET',
+			      	  url: url+"/notas/detalle_nota_usuario/"+id,
+			      	  headers: {"token":InfoUsuario.token},
+			      	}).then(function successCallback(response) {
+			          	$scope.id_nota=response.data.idNota;
+			          	$scope.titulo=response.data.titulo;
+			          	$scope.contenido=response.data.contenido;
+			      	}, function errorCallback(response) {
+			      		 console.log(response.data.status);
+			      	});
+			}
+			else{
+	          	$scope.id_nota=0;
+	          	$scope.titulo="";
+	          	$scope.contenido="";
+			}
 		}
 		if(elemento=="lista"){
 			$scope.plantilla='modals/lista.html';
 			$scope.modal={"titulo":"Lista"};
+			$scope.id_lista=0;
 		}
 		
 	}
 })
 
 listoplan.controller('notaController',function($scope,$http,InfoUsuario){
-	$scope.guardarNota=function(){
+	$scope.guardarNota=function(id_nota){
 		$("#error_nota").hide();
 		$("#ok_nota").hide();
 		if($scope.titulo==undefined || $scope.titulo.length==0){
@@ -77,12 +81,20 @@ listoplan.controller('notaController',function($scope,$http,InfoUsuario){
 			$scope.error_msg="La nota debe tener un título";
 			return;
 		}
+		var reqUrl;
+		if(id_nota==0){
+			//Nueva nota
+			reqUrl=url+"/notas/nueva_nota";
+		}else{
+			reqUrl=url+"/notas/modificacion_nota"
+		}
 	    $http({
 	    	  method: 'POST',
-	    	  url: url+"/notas/nueva_nota",
+	    	  url: reqUrl,
 	    	  headers: {"token":InfoUsuario.token},
 	    	  data:{
 	    		  "id":InfoUsuario.id_usuario.toString(),
+	    		  "idNota":id_nota.toString(),
 	    		  "titulo":$scope.titulo,
 	    		  "contenido":$scope.contenido,
 	    		  "ambito":"usuario"
@@ -96,8 +108,48 @@ listoplan.controller('notaController',function($scope,$http,InfoUsuario){
 	  				console.log(response.data.status);
 	    	    	
 	    	  });
+		};
+	    $scope.getNotasUsuario=function(){
+	        $http({
+	      	  method: 'GET',
+	      	  url: url+"/notas/notas_usuario",
+	      	  headers: {"token":InfoUsuario.token},
+	      	}).then(function successCallback(response) {
+	          	$scope.notas=response.data;
+	      	}, function errorCallback(response) {
+	      		 console.log(response.data.status);
+	      	});
+	    };
+	    $scope.desactivarNota=function(id_nota){
+	    	if(confirm("Estás seguro que deseas eliminar la nota?") && id_nota > 0){
+	    		$http({
+	  	    	  method: 'POST',
+	  	    	  url: url+"/notas/desactivacion_nota",
+	  	    	  headers: {"token":InfoUsuario.token},
+	  	    	  data:{
+	  	    		  "id":InfoUsuario.id_usuario.toString(),
+	  	    		  "idNota":id_nota.toString(),
+	  	    		  "ambito":"usuario"
+	  	    	  }
+	  	    	}).then(function successCallback(response) {
+	  		        	alert("La nota se ha eliminado correctamente");
+	  		        	$('#modal_generico').modal('toggle');
+	  	    	  }, function errorCallback(response) {
+	  	  				$("#error_nota").show();
+	  	  				$scope.error_msg="Se ha producido un error al eliminar la nota";
+	  	  				console.log(response.data.status);
+	  	    	    	
+	  	    	  });
+	    	}else{
+	    		return;
+	    	}
+	    	
+	    }
 
-	}
+});
+
+listoplan.controller('dashboardController', function($scope){
+	$scope.vistaDashboard="fragmentos/items_usuario.html";
 });
 
 $("#logout").click(function(evento){
@@ -106,8 +158,89 @@ $("#logout").click(function(evento){
 	window.location.replace("login.html");
 });
 
-/*$("#nueva_nota").click(function(evento){
-	evento.preventDefault();
-	$("#contenido_modal").load("modals/nota.html");
-	$("#contenido_")
-});*/
+listoplan.controller('listaController',function($scope,$http,InfoUsuario){
+	$scope.guardarLista=function(id_lista){
+		$("#error_lista").hide();
+		$("#ok_lista").hide();
+		if($scope.nombre==undefined || $scope.nombre.length==0){
+			$("#error_lista").show();
+			$scope.error_msg="La lista debe tener un nombre";
+			return;
+		}
+		if($scope.tipo_lista==undefined || $scope.tipo_lista==null || $scope.tipo_lista.length==0){
+			$("#error_lista").show();
+			$scope.error_msg="La lista debe tener un tipo";
+			return;
+		}
+		if($scope.descripcion==undefined || $scope.descripcion==null || $scope.descripcion.length==0){
+			$("#error_lista").show();
+			$scope.error_msg="La lista debe tener una descripción";
+			return;
+		}
+		var reqUrl;
+		if(id_lista==0){
+			//Nueva nota
+			reqUrl=url+"listas/nueva_lista";
+		}else{
+			reqUrl=url+"listas/modificacion_lista"
+		}
+	    $http({
+	    	  method: 'POST',
+	    	  url: reqUrl,
+	    	  headers: {"token":InfoUsuario.token},
+	    	  data:{
+	    		  "id":InfoUsuario.id_usuario.toString(),
+	    		  "idLista":id_lista.toString(),
+	    		  "nombre":$scope.nombre,
+	    		  "descripcion":$scope.descripcion,
+	    		  "tipoLista":$scope.tipo_lista,
+	    		  "ambito":"usuario"
+	    	  }
+	    	}).then(function successCallback(response) {
+		        	$("#ok_lista").show();
+		        	$scope.ok_msg="La lista se ha guardado correctamente";
+	    	  }, function errorCallback(response) {
+	  				$("#error_nota").show();
+	  				$scope.error_msg="Se ha producido un error al guardar la lista";
+	  				console.log(response.data.status);
+	    	    	
+	    	  });
+		};
+	    $scope.getListasUsuario=function(){
+	        $http({
+	      	  method: 'GET',
+	      	  url: url+"listas/listas_usuario/",
+	      	  headers: {"token":InfoUsuario.token},
+	      	}).then(function successCallback(response) {
+	          	$scope.listas=response.data;
+	      	}, function errorCallback(response) {
+	      		 console.log(response.data.status);
+	      	});
+	    };
+	    $scope.desactivarNota=function(id_nota){
+	    	if(confirm("Estás seguro que deseas eliminar la nota?") && id_nota > 0){
+	    		$http({
+	  	    	  method: 'POST',
+	  	    	  url: url+"/notas/desactivacion_nota",
+	  	    	  headers: {"token":InfoUsuario.token},
+	  	    	  data:{
+	  	    		  "id":InfoUsuario.id_usuario.toString(),
+	  	    		  "idNota":id_nota.toString(),
+	  	    		  "ambito":"usuario"
+	  	    	  }
+	  	    	}).then(function successCallback(response) {
+	  		        	alert("La nota se ha eliminado correctamente");
+	  		        	$('#modal_generico').modal('toggle');
+	  	    	  }, function errorCallback(response) {
+	  	  				$("#error_nota").show();
+	  	  				$scope.error_msg="Se ha producido un error al eliminar la nota";
+	  	  				console.log(response.data.status);
+	  	    	    	
+	  	    	  });
+	    	}else{
+	    		return;
+	    	}
+	    	
+	    }
+
+});
